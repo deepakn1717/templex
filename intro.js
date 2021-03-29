063301186f2b832df1a37799007b5cf08ce44515
deepakn1717 <deepakn1717@gmail.com>
2021-03-29 09:12:13 -0400

    added intro

---
+++ intro.js
@@ -0,0 +1,20 @@
+//step1: clone or downlaod the applicaiton
+//step2: npm i
+//step3 node index.js
+
+
+//Through command line, user need to select one of the options from mainOptionsMenu (try othr options you will get menu list again)
+//next, select one or more from all template, visuals and tags (sample 1,4,5)
+//once all user inpus were given, engineRules will execute the input and predicts best options. 
+
+//please select folowing options to get optput: 
+//-> business
+//-> visuals ==> 6
+//-> outline ==> 13
+//-> tags ==> 1
+
+//Note: did not get time to add more rules,  if get time, next steps were 
+//1) add defalut template
+//2) add more engineRules
+//3) Joi validatins etc
+
